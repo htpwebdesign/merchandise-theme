11c2b2d8cc2c3900daf74c1a01e6595da9c0ad30
Balpreet-Dhillon <balpreetd@live.ca>
2022-10-28 11:49:56 -0700

    Create front-page.php

---
+++ front-page.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Merchandise_Theme
+ */
+
+get_header();
+?>
+
+	<main id="primary" class="site-main">
+
+		<?php
+		while ( have_posts() ) :
+			the_post();
+
+			get_template_part( 'template-parts/content', 'page' );
+
+			if ( function_exists( 'get_field' ) ) {
+				if ( get_field( 'home_message' ) ) {
+					$image = get_field('home_message');
+					$size = 'full'; // (thumbnail, medium, large, full or custom size)
+					if( $image ) {
+						echo wp_get_attachment_image( $image, $size );
+
+						
+
+					}
+					
+				}
+
+				$link = get_field('mens');
+if( $link ): 
+    $link_url = $link['url'];
+    $link_title = $link['title'];
+    $link_target = $link['target'] ? $link['target'] : '_self';
+    ?>
+    <a class="button" href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>"><?php echo esc_html( $link_title ); ?></a>
+
+	<?php endif; 
+
+		endwhile; // End of the loop.
+
+
+		?>
+		
+
+
+	</main><!-- #main -->
+
+<?php
+get_sidebar();
+get_footer();
