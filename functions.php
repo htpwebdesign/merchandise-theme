e345480dac1a9d0639199a3112deb454596a2d87
SufiyanAhmed1965 <116763899+SufiyanAhmed1965@users.noreply.github.com>
2022-11-02 12:55:27 -0700

    New Styles Files
    new style files created

--- functions.php
+++ functions.php
@@ -194,3 +194,36 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 if ( class_exists( 'WooCommerce' ) ) {
 	require get_template_directory() . '/inc/woocommerce.php';
 }
+function my_login_logo() { ?>
+    <style type="text/css">
+        #login h1 a, .login h1 a {
+            background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/images/picture1.png);
+		
+			
+		height:65px;
+		width:320px;
+		background-size: 320px 65px;
+		background-repeat: no-repeat;
+		    
+
+
+        }
+    </style>
+<?php }
+add_action( 'login_enqueue_scripts', 'my_login_logo' );
+
+function my_login_logo_url() {
+    return home_url();
+}
+add_filter( 'login_headerurl', 'my_login_logo_url' );
+
+function my_login_logo_url_title() {
+    return 'YVR';
+}
+add_filter( 'login_headertext', 'my_login_logo_url_title' );
+
+function my_login_stylesheet() {
+    wp_enqueue_style( 'custom-login', get_stylesheet_directory_uri() . '/style-login.css' );
+   
+}
+add_action( 'login_enqueue_scripts', 'my_login_stylesheet' );
